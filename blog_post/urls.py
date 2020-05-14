126a455f544b78d6db8527923044664360a0c8c4
Manoj Panda <kumarpandamanoja@gmail.com>
2020-05-14 20:34:37 +0530

    This is for the addition of the new feature for the admin person

--- blog_post/urls.py
+++ blog_post/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 app_name='Blog_post'
 urlpatterns=[
-    path('',views.Blog_Post.as_view(),name='Create_blog'),
+    path('',views.Home_page.as_view(),name='Home_Page'),
+    path('blog-post/',views.Blog_Post.as_view(),name='Create_blog'),
     path('list/',views.List_blog.as_view(),name='List_blog'),
-    path('<str:slug>/detail/',views.Detail_blog.as_view(),name='Detail_blog'),
+    path('<str:slug>/detail/',views.Detail_blog.as_view(),kwargs={'view':'Detail_page'},name='Detail_blog'),
+    path('<str:slug>/edit/',views.Detail_blog.as_view(),kwargs={'view':'Edit_page'},name='Edit_blog'),
 ]
