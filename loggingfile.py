3748adcf3bf18fff421730b9556bbaaa19c697a2
Manoja Kumar Panda <54284976+manojakumarpanda@users.noreply.github.com>
2020-08-03 19:43:29 +0530

    Loggfile
    Limiting the size of the log file

--- loggingfile.py
+++ loggingfile.py
@@ -10,14 +10,18 @@ LOGGING = {
             'level': 'INFO',
             'class': 'logging.FileHandler',
             'filename':os.path.join(BASE_DIR,'djangoErrorLog.log') ,
-            'formatter':'formats',
+            'formatters':'formats',
+            'backupCount': 10, # keep at most 10 log files
+            'maxBytes': 5242880, # 5*1024*1024 bytes (5MB)
             },
         'error':{
             'level':'ERROR',
             'class': 'logging.FileHandler',
             'class':'logging.raiseExceptions',
             'filename':os.path.join(BASE_DIR,'djangoErrorLog.log'),
-            'formatter':'formats',
+            'formatters':'formats',
+            'backupCount': 10, # keep at most 10 log files
+            'maxBytes': 5242880, # 5*1024*1024 bytes (5MB)
         },
     },
     'loggers': {
